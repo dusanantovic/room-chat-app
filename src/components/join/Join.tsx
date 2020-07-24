821995aedc78c40234cd44b07432ec594d20765e
Dusan Antovic <dusan@pyrotree.com>
2020-07-24 23:02:35 +0200

    css

--- src/components/join/Join.tsx
+++ src/components/join/Join.tsx
@@ -9,7 +9,7 @@ const styles = (theme: Theme) => ({
         display: "flex",
         alignItems: "center",
         width: "100%",
-        height: "100vh"
+        height: "100%"
     },
     center: {
         display: "flex",
